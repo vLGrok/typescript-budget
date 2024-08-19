bfdd92da16394e5b800e5e44fe34f01cbd98c544
Robert Kerr <robert.kerr@zelis.com>
2024-08-18 22:43:07 -0400

    define interest rate types or categories

---
+++ src/enums/InterestRateType.ts
@@ -0,0 +1,8 @@
+// src/enums/InterestRateType.ts
+export enum InterestRateType {
+    NormalPurchase = "NormalPurchase",        // Standard interest rate for purchases
+    CashAdvance = "CashAdvance",              // Interest rate for cash advances
+    BalanceTransferPromo = "BalanceTransferPromo", // Promotional rate for balance transfers
+    BalanceTransferPostPromo = "BalanceTransferPostPromo", // Rate after promo for balance transfers
+    Promotional = "Promotional",              // General promotional rate, can be specific
+  }
